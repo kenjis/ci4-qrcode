b8a6fe51223db1ca3a84a459e806e97ab61c0ab7
kenjis <kenji.uui@gmail.com>
2021-01-27 14:56:58 +0900

    chore: php spark revision:update

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -10,8 +10,6 @@ use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
 /**
- * Class BaseController
- *
  * BaseController provides a convenient place for loading components
  * and performing functions that are needed by all your controllers.
  * Extend this class in any new controllers:
