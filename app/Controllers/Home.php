f0d7cbf7410542984e6d5a6f95808cdf284ba614
kenjis <kenji.uui@gmail.com>
2021-02-10 15:41:01 +0900

    migrate: copy controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,13 +1,154 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-declare(strict_types=1);
+class Home extends MY_Controller {
 
-namespace App\Controllers;
-
-class Home extends BaseController
-{
-    public function index(): string
+    /*
+    |-------------------------------------------------------------------
+    | Construct
+    |-------------------------------------------------------------------
+    | 
+    */
+    function __construct()
     {
-        return view('welcome_message');
+        parent::__construct();
+        $this->load->model('home_model');
     }
+
+    /*
+    |-------------------------------------------------------------------
+    | Index
+    |-------------------------------------------------------------------
+    |
+    */
+	function index()
+	{
+        $data['title']   = 'Codeigniter 3 - QR Code';
+        $data['qr_list'] = $this->home_model->fetch_datas();
+
+        $this->load->view('frontend/header', $data);
+        $this->load->view('frontend/content', $data);
+        $this->load->view('frontend/footer', $data);
+    }
+    
+    /*
+    |-------------------------------------------------------------------
+    | Generate QR Code
+    |-------------------------------------------------------------------
+    |
+    | @param $data   QR Content
+    |
+    */
+	function generate_qrcode($data)
+	{
+        /* Load QR Code Library */
+        $this->load->library('ciqrcode');
+        
+        /* Data */
+        $hex_data   = bin2hex($data);
+        $save_name  = $hex_data.'.png';
+
+        /* QR Code File Directory Initialize */
+        $dir = 'assets/media/qrcode/';
+        if (!file_exists($dir)) {
+            mkdir($dir, 0775, true);
+        }
+
+        /* QR Configuration  */
+        $config['cacheable']    = true;
+        $config['imagedir']     = $dir;
+        $config['quality']      = true;
+        $config['size']         = '1024';
+        $config['black']        = array(255,255,255);
+        $config['white']        = array(255,255,255);
+        $this->ciqrcode->initialize($config);
+  
+        /* QR Data  */
+        $params['data']     = $data;
+        $params['level']    = 'L';
+        $params['size']     = 10;
+        $params['savename'] = FCPATH.$config['imagedir']. $save_name;
+        
+        $this->ciqrcode->generate($params);
+
+        /* Return Data */
+        $return = array(
+            'content' => $data,
+            'file'    => $dir. $save_name
+        );
+        return $return;
+    }
+    
+    /*
+    |-------------------------------------------------------------------
+    | Add Data
+    |-------------------------------------------------------------------
+    |
+    */
+	function add_data()
+	{
+        /* Generate QR Code */
+        $data = $this->input->post('content');
+        $qr   = $this->generate_qrcode($data);
+
+        /* Add Data */
+        if($this->home_model->insert_data($qr)) {
+            $this->modal_feedback('success', 'Success', 'Add Data Success', 'OK');
+        } else {
+            $this->modal_feedback('error', 'Error', 'Add Data Failed', 'Try again');
+        }
+        redirect('/');
+
+    }
+
+    /*
+    |-------------------------------------------------------------------
+    | Edit Data
+    |-------------------------------------------------------------------
+    |
+    | @param $id    ID Data
+    |
+    */
+	function edit_data($id)
+	{
+        /* Old QR Data */
+        $old_data = $this->home_model->fetch_data($id);
+        $old_file = $old_data['file'];
+
+        /* Generate New QR Code */
+        $data = $this->input->post('content');
+        $qr   = $this->generate_qrcode($data);
+
+        /* Edit Data */
+        if($this->home_model->update_data($id, $old_file, $qr)) {
+            $this->modal_feedback('success', 'Success', 'Edit Data Success', 'OK');
+        } else {
+            $this->modal_feedback('error', 'Error', 'Edit Data Failed', 'Try again');
+        }
+        redirect('/');
+    }
+
+    /*
+    |-------------------------------------------------------------------
+    | Remove Data
+    |-------------------------------------------------------------------
+    |
+    | @param $id    ID Data
+    |
+    */
+	function remove_data($id)
+	{
+        /* Current QR Data */
+        $qr_data = $this->home_model->fetch_data($id);
+        $qr_file = $qr_data['file'];
+
+        /* Delete Data */
+        if($this->home_model->delete_data($id, $qr_file)) {
+            $this->modal_feedback('success', 'Success', 'Delete Data Success', 'OK');
+        } else {
+            $this->modal_feedback('error', 'Error', 'Delete Data Failed', 'Try again');
+        }
+        redirect('/');
+	}
+    
 }
